201a943ddb41ed715a771c07ce1beb0b0baceb22
PaulLeichtfried <leichtfriedpaul@gmail.com>
2017-07-31 17:02:40 +0200

    added isValidEffect method

--- src/guncreator/utils/EnumChecker.java
+++ src/guncreator/utils/EnumChecker.java
@@ -1,5 +1,6 @@
 package guncreator.utils;
 
+import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 
@@ -37,4 +38,16 @@ public class EnumChecker {
 		
 	}
 	
+	public static boolean isValidEffect(String effect) {
+		try {
+			Effect.valueOf(effect);
+			
+			return true;
+			
+		} catch (IllegalArgumentException e) {
+			
+			return false;
+			
+		}
+	}
 }
