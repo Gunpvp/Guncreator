eb773fad433dc873c1ebdb7213d35e7791637f50
PaulLeichtfried <leichtfriedpaul@gmail.com>
2017-07-31 17:45:41 +0200

    finished scope category

--- src/guncreator/categories/ScopeCategory.java
+++ src/guncreator/categories/ScopeCategory.java
@@ -1,21 +1,13 @@
 package guncreator.categories;
 
-import java.awt.Color;
-
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
-import javax.swing.JSlider;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
 import guns.weopons.data.ScopeData;
 
 public class ScopeCategory extends Category<ScopeData>{
 
-	private JCheckBox scope;
-	private JCheckBox see_in_night;
-	private JSlider zoom_amount;
-	private JSlider spread;
+	private JBox scope;
+	private JBox see_in_night;
+	private JAdjustbar zoom_amount;
+	private JAdjustbar spread;
 	private JGunSound toggle_sound;
 	
 	public ScopeCategory() {
@@ -31,72 +23,34 @@ public class ScopeCategory extends Category<ScopeData>{
 			@Override
 			protected ScopeData generateData() {
 				
-				return new ScopeData(scope.isSelected(), zoom_amount.getValue(), see_in_night.isSelected(), spread.getValue()/10, toggle_sound.getGunSound());
+				return new ScopeData(scope.getValue(), ((int)zoom_amount.getValue()), see_in_night.getValue(), spread.getValue(), toggle_sound.getGunSound());
 			}
 
 			@Override
 			public void initWithData(ScopeData data) {
-				
-				
+				scope.setValue(data.isScopeable());
+				see_in_night.setValue(data.isSeeingInNight());
+				zoom_amount.setValue(data.getZoomAmount());
+				spread.setValue(data.getSpread());
+				toggle_sound.setSound(data.getToggleSound());
 			}
 			
 		};
 		 
-		panel.setLayout(null);
-	
-		//scope (zoom) available
-		JLabel text_scope = new JLabel("Zoom möglich:");
-		text_scope.setBounds(10, 10, 150, 30);
-		panel.add(text_scope);
-		scope = new JCheckBox();
-		scope.setBackground(Color.GREEN);
-		scope.setBounds(200, 10, 30, 30);
-		panel.add(scope);
-		
-		//zoom amount
-		JLabel text_zoom_amount = new JLabel("Zoomweite:");
-		text_zoom_amount.setBounds(10, 60, 150, 30);
-		panel.add(text_zoom_amount);
-		zoom_amount = new JSlider(0,20);
-		zoom_amount.setBackground(Color.GREEN);
-		zoom_amount.setBounds(200, 60, 200, 30);
-		panel.add(zoom_amount);
-		zoom_amount.addChangeListener(new ChangeListener() {
-	        public void stateChanged(ChangeEvent ce) {
-	            JSlider slider = (JSlider)ce.getSource();
-	            text_zoom_amount.setText("Zoomweite: " + slider.getValue());
-	        }
-		});
-		
-		//night vision
-		JLabel text_see_in_night = new JLabel("Nachtsicht:");
-		text_see_in_night.setBounds(10, 110, 150, 30);
-		panel.add(text_see_in_night);
-		see_in_night = new JCheckBox();
-		see_in_night.setBackground(Color.GREEN);
-		see_in_night.setBounds(200, 110, 30, 30);
-		panel.add(see_in_night);
-		
-		//spread
-		JLabel text_spread = new JLabel("Streuung:");
-		text_spread.setBounds(10, 160, 150, 30);
-		panel.add(text_spread);
-		spread = new JSlider(0,500);
-		spread.setBackground(Color.GREEN);
-		spread.setBounds(200, 160, 200, 30);
-		panel.add(spread);
-		spread.addChangeListener(new ChangeListener() {
-	        public void stateChanged(ChangeEvent ce) {
-	            JSlider slider = (JSlider)ce.getSource();
-	            text_spread.setText("Streuung: " + slider.getValue()/10);
-	        }
-		});
-		
-		//toggle sound
-		toggle_sound = new JGunSound("Toggle");
-		toggle_sound.setBounds(10, 210, 400, 300);
-		toggle_sound.setBackground(Color.GREEN);
-		panel.add(toggle_sound);
+			JBox scope = new JBox("Waffe kann zoomen");
+			panel.addComponent(scope);
+			
+			JAdjustbar zoom_amount = new JAdjustbar("Zoomstärke", "", 0, 20, 1);
+			panel.addComponent(zoom_amount);
+			
+			JBox see_in_night = new JBox("Zoom hat Nachtsicht");
+			panel.addComponent(see_in_night);
+			
+			JAdjustbar spread = new JAdjustbar("Streuung", "", 0, 5, 0.1f);
+			panel.addComponent(spread);
+			
+			JGunSound toggle_sound = new JGunSound("Umschalt Sound");
+			panel.addComponent(toggle_sound);
 		
 		return panel;
 	}
@@ -104,7 +58,7 @@ public class ScopeCategory extends Category<ScopeData>{
 	@Override
 	public boolean isEverythingFilledOut() {
 
-		return false;
+		return toggle_sound.isDataValid();
 	}
 
 }
